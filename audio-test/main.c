357a579601e2befc5764be0a22e35101acafbb43
Phil <philipcumner@gmail.com>
2017-05-05 14:41:38 +0100

    Start sine wave function

--- audio-test/main.c
+++ audio-test/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-main(void) {
-	
+#define SAMPLE_RATE 50
+#define CYCLE_LENGTH 1		// in seconds
+
+int main(void) {
+	float phase = 0;
+	for (int i = 0; i <= SAMPLE_RATE; i++) {
+		float y = sin(phase);
+
+		phase = phase + ((2 * M_PI) / SAMPLE_RATE);
+
+		// if (phase > (2 * M_PI)) {
+		// 	phase = phase - (2 * M_PI);
+		// }
+
+		printf("%f, %f\n", phase, y);
+	}
 }
